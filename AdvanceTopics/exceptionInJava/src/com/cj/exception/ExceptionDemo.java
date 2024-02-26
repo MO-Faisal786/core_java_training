41e215f9f864d84fabf0e9847ead0f07e8393dab
MO-Faisal786 <fm704034@gmail.com>
2024-02-26 21:36:15 +0530

    some changes to practice

--- AdvanceTopics/exceptionInJava/src/com/cj/exception/ExceptionDemo.java
+++ AdvanceTopics/exceptionInJava/src/com/cj/exception/ExceptionDemo.java
@@ -3,9 +3,8 @@ package com.cj.exception;
 public class ExceptionDemo {
 	public static void main(String[] args) {
 		System.out.println("Faisal Malik");
-<<<<<<< HEAD
 		System.out.println("Faisal Malik");
-=======
->>>>>>> 3cab7c407a26cf6b421aa9573f919cd4803c0768
+		System.out.println("Faisal Malik");
+		System.out.println("Faisal Malik");
 	}
 }
