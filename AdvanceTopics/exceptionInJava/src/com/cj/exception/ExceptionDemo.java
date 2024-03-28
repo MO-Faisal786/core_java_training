42a0209a823b2a4ff0c775575d99783c49deabd1
MO-Faisal786 <fm704034@gmail.com>
2024-03-28 10:51:42 +0530

    some changes to test

--- AdvanceTopics/exceptionInJava/src/com/cj/exception/ExceptionDemo.java
+++ AdvanceTopics/exceptionInJava/src/com/cj/exception/ExceptionDemo.java
@@ -2,9 +2,6 @@ package com.cj.exception;
 
 public class ExceptionDemo {
 	public static void main(String[] args) {
-		System.out.println("Faisal Malik");
-		System.out.println("Faisal Malik");
-		System.out.println("Faisal Malik");
-		System.out.println("Faisal Malik");
+		
 	}
 }
